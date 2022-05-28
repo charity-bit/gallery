4a16fb5fc32511f670353b78fd15d6505ce6dd34
charity-bit <charitynyanchera@gmail.com>
2022-05-28 12:42:36 +0300

    added boilerplate for the project

---
+++ Makefile
@@ -0,0 +1,14 @@
+serve:
+	python3 manage.py runserver
+
+migrate:
+	python3 manage.py migrate
+
+check:
+	python3 manage.py check
+
+shell:
+	python3 manage.py shell
+
+makemigrations:
+	python3 manage.py makemigrations
--- app/settings.py
+++ app/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'gallery.apps.GalleryConfig'
+    'gallery.apps.GalleryConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
