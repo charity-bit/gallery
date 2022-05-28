154ecbfc2ac7387152a62da5741bdb96d0e00f57
charity-bit <charitynyanchera@gmail.com>
2022-05-28 13:00:04 +0300

    ft:models -added models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,48 @@
+from distutils.command.upload import upload
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    def save_locaton(self):
+        self.save()
+
+
+
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    def save_category(self):
+        self.save()
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'pictures/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    location = models.ForeignKey(Location,on_delete=CASCADE)
+
+
+    def save_image(self):
+        self.save()
+
+
+    def __str__(self):
+        return self.name + self.description
+
+
