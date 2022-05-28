477776f1f6d86bd92228c0a72b30fa8fc49f6fdf
charity-bit <charitynyanchera@gmail.com>
2022-05-29 02:24:04 +0300

    st: navbar

--- gallery/static/gallery/js/main.js
+++ gallery/static/gallery/js/main.js
@@ -37,3 +37,4 @@ input.addEventListener('change',(e)=>{
 
 
 
+
---
+++ gallery/static/gallery/js/modal.js
@@ -0,0 +1,20 @@
+const allItems = document.querySelectorAll('.item');
+const detail = document.querySelectorAll(`.content`);
+
+
+
+allItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+        
+        detail.forEach(item => {
+            alert(item.innerHTML);
+        })
+        
+
+    })
+
+    
+});
+
+
+
